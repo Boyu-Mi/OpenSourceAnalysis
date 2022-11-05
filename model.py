4fe0f2ed9606ab0fae75a7e8beed7cf1a606d773
miboyu <3200102888@zju.edu.cn>
2022-11-05 14:49:51 +0800

    上传了基础接口的TODO文档

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sa
 from flask_sqlalchemy import SQLAlchemy
+
 db = SQLAlchemy()
 
 
@@ -28,5 +29,5 @@ class date01(db.Model):
                    nullable=False)
     repo_name = sa.Column(sa.String(128), nullable=False, default='')
     date_newest = sa.Column(sa.DateTime)
-    date_local = sa.Column(sa.DateTime)  #目前还不知道自动设置更新时间的方法
+    date_local = sa.Column(sa.DateTime)  # 目前还不知道自动设置更新时间的方法
     date_lasttime = sa.Column(sa.DateTime)
